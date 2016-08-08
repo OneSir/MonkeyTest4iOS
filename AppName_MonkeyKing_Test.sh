77b0e85c298d359527b3c7b75fea68ad453b1ca2
One <wny.china@foxmail.com>
2016-08-08 16:38:35 +0800

    Update AppName_MonkeyKing_Test.sh

--- AppName_MonkeyKing_Test.sh
+++ AppName_MonkeyKing_Test.sh
@@ -9,4 +9,4 @@ export PATH=$PATH:/Library/Ruby/Gems/2.0.0/gems/smart_monkey-0.5.0/bin/
 instruments -w 设备号 -t "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.xrplugin/Contents/Resources/Automation.tracetemplate" com.xxx.iphone -e UIASCRIPT /Users/用户名/登录脚本路径/APPLogin/xxx.js
 
 #执行稳定性测试
-smart_monkey -a com.xxx.iphone -w 设备号 --compress-result 50% -n 1 -s /Users/用户名/Desktop/LeFeng_Crash/APPdSYM文件名.app.dSYM
+smart_monkey -a com.xxx.iphone -w 设备号 --compress-result 50% -n 1 -s /Users/用户名/Desktop/APP的dSYM文件路径/APPdSYM文件名.app.dSYM
